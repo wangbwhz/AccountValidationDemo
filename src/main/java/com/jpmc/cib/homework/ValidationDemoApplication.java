d35f630a451e32042883aa73a8f52a503b94ca4d
wang.bowen <wangbwhz99@gmail.com>
2021-05-30 20:20:37 +0800

    JIRA-0000 generate project by spring initializr

---
+++ src/main/java/com/jpmc/cib/homework/ValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jpmc.cib.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpmc/cib/homework/ValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmc.cib.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
